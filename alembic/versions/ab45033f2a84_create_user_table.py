2e3a36f98621b6cc5d576857316a3b7b6359dc1a
Lara Filippone <lara.filippone@gmail.com>
2024-10-05 16:50:03 +0200

    Add user migration

---
+++ alembic/versions/ab45033f2a84_create_user_table.py
@@ -0,0 +1,40 @@
+"""create user table
+
+Revision ID: ab45033f2a84
+Revises: 32fdc07746bd
+Create Date: 2024-10-04 19:51:36.207660
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'ab45033f2a84'
+down_revision: Union[str, None] = '32fdc07746bd'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", primary_key=True, autoincrement=True),
+        sa.Column("first_name", sa.String(20), nullable=False),
+        sa.Column("last_name", sa.String(20), nullable=False),
+        sa.Column("birthdate", sa.DateTime, nullable=False),
+        sa.Column("cf", sa.String(16), nullable=False),
+        sa.Column("address", sa.Column(30), nullable=False),
+        sa.Column("city", sa.Column(30), nullable=False),
+        sa.Column("cap", sa.Integer, nullable=True, server_default=None),
+        sa.Column("state", sa.String(20), nullable=True, server_default=None),
+        sa.Column("telephone", sa.Integer, nullable=True, server_default=None),
+        sa.Column("email", sa.String(30), nullable=True, server_default=None),
+        sa.Column("documents", sa.Integer, nullable=True, server_default=None)
+    )
+# first_name, last_name, birthday, sex, cf, street, cap, city, state, telephone, email, documents
+
+def downgrade() -> None:
+    op.drop_table("user")
